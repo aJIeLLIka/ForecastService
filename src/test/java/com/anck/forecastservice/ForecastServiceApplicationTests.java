ced4c50540e20885bd3adfb4afecee5d4859f7d0
Alexei <blatnoi911@mail.ru>
2023-03-24 18:40:56 +0300

    first commit

---
+++ src/test/java/com/anck/forecastservice/ForecastServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anck.forecastservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForecastServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
